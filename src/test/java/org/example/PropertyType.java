c3a5fbb58b578c0d09b095646fec0c5f4260446a
Robertar <robertandrei.neagu@yahoo.com>
2025-04-16 09:05:07 +0300

    C3

--- src/test/java/org/example/PropertyType.java
+++ src/test/java/org/example/PropertyType.java
@@ -5,13 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.asserts.SoftAssert;
 
-import java.awt.*;
 import java.time.Duration;
 
 
@@ -19,7 +14,7 @@ import static org.junit.Assert.*;
 
 
 
-public class tipProprietate{
+public class PropertyType {
     WebDriver driver;
 
     //Wait
@@ -52,14 +47,14 @@ public class tipProprietate{
         assertEquals("Booking.com | Official site | The best hotels, flights, car rentals & accommodations", title);
 
         if (title.contains("Booking.com | Official site | The best hotels, flights, car rentals & accommodations")) {
-            System.out.println("Suntem pe pagina Booking.com");
+            System.out.println("We are on the  Booking.com page! ");
         } else {
-            System.out.println("Nu suntem pe pagina Booking.com");
+            System.out.println("We are not on the Booking.com page! ");
         }
 
         WebElement acceptcokie = driver.findElement(By.cssSelector("#onetrust-accept-btn-handler"));
-        assertTrue("Butonul nu este vizibil", acceptcokie.isDisplayed());
-        assertTrue("Butonul nu uncitoneaza", acceptcokie.isEnabled());
+        assertTrue("The button is not visible! ", acceptcokie.isDisplayed());
+        assertTrue("The button is not working! ", acceptcokie.isEnabled());
         acceptcokie.click();
 
 
@@ -68,7 +63,7 @@ public class tipProprietate{
         WebElement Hotels = driver.findElement(By.cssSelector("#\\:r1i\\: > li:nth-child(1) > a > div > div.f2cf178bcd.c3a4d3369f > h3"));
 
         //validam cu assert ca optiunea este vizibila
-        assertTrue("optiunea nu apare", Hotels.isDisplayed());
+        assertTrue("The option is not visible! ", Hotels.isDisplayed());
 
 
         //scroll down
@@ -87,14 +82,14 @@ public class tipProprietate{
             popupsigninClose.click();
         }
         catch(Exception e){
-            System.out.println("Pop-up nu a aparut");
+            System.out.println("Pop-up is not displayed! ");
 
         }
 
         //seelecteaza hotelul
 
         WebElement selecthotel= driver.findElement(By.cssSelector("#bodyconstraint-inner > div.lp-bui-section-wrap > div:nth-child(1) > div > div > div:nth-child(2) > div > div:nth-child(1) > div.ecc9a4771b > div.fe5b3e7dac > a > div > h3"));
-        assertTrue("hotelul nu este pe pagina", selecthotel.isDisplayed());
+        assertTrue("The hotel is not displayed on the page! ", selecthotel.isDisplayed());
         selecthotel.click();
 
         //apasam btonul rezervati acum
@@ -102,9 +97,11 @@ public class tipProprietate{
         WebElement RezervatiAcum= driver.findElement(By.cssSelector("#hcta > span.bui-button__text"));
         RezervatiAcum.click();
 
-        assertTrue("Pagina cu detaliile pentru rezervare nu a fost deschisa", RezervatiAcum.isDisplayed());
+        assertTrue(" The reservation details page is not open! ", RezervatiAcum.isDisplayed());
 
     }
+
+
     @After
     public void teardown() {
         driver.quit();
